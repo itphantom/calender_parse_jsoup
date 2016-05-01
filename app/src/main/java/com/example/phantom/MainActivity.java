b84a458b96d51ca95572f179e7eeec2af122b99a
itphantom <hw61102@gmail.com>
2016-05-02 06:16:53 +0900

    ㅎㅎ

--- app/src/main/java/com/example/phantom/MainActivity.java
+++ app/src/main/java/com/example/phantom/MainActivity.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {    
 private Elements roopp;
-    private String htmlPageUrl = "http://duckso.hs.kr/calendar.list?ym=201605";    
+    private String htmlPageUrl = "http://duckso.hs.kr/board.list?mcode=1110?mcode=1110";    
     private TextView textviewHtmlDocument;    
 	private TextView textviewHtmlDocument2;
     private String htmlContentInStringFormat;    
@@ -32,8 +32,6 @@ private Elements roopp;
 
         textviewHtmlDocument = (TextView)findViewById(R.id.textView);    
         textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod());    
-		textviewHtmlDocument2 = (TextView)findViewById(R.id.textView2);    
-        textviewHtmlDocument2.setMovementMethod(new ScrollingMovementMethod());    
 		
         Button htmlTitleButton = (Button)findViewById(R.id.button);    
         htmlTitleButton.setOnClickListener(new View.OnClickListener() {    
@@ -56,22 +54,12 @@ private Elements roopp;
         protected Void doInBackground(Void... params) {    
             try {    
                 Document doc = Jsoup.connect(htmlPageUrl).get();    
-                Elements links = doc.select("#contentbox");    
-				Elements links2 = links.select("td");
+                Elements links = doc.select("tbody");    
+				Elements links2 = links.select("tr");		
+				for(Element kk:links2){
+					kk.select("a");
 				
-				Element sw;
-				
-					for(Element kkk:links2){
-						if(kkk.toString().contains("<ul>")){
-					
-					sw=kkk.select("span").first();
-							htmlContentInStringFormat2 += sw.text().trim()+"일\n";
-				}
-					
-				}
-			Elements links3 = links2.select("a[href]");
-                for (Element link : links3) {    
-                    htmlContentInStringFormat += "("+link.text().trim() + ")\n";    
+                    htmlContentInStringFormat += "("+kk.text().trim() + ")\n";    
                 }    
 
             } catch (IOException e) {    
@@ -83,9 +71,8 @@ private Elements roopp;
 
         @Override    
         protected void onPostExecute(Void result) {    
-            textviewHtmlDocument.setText(htmlContentInStringFormat.replaceAll("null",""));    
-			textviewHtmlDocument2.setText(htmlContentInStringFormat2.replaceAll("null",""));
-        }    
+            textviewHtmlDocument.setText(htmlContentInStringFormat);    
+			}
     }    
 
 
